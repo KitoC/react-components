4956d74a7c59c40c9be2b205c37281a8384f39ea
kito.clark <kitoclark1989@gmail.com>
2022-06-14 07:29:34 -0500

    updated rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,33 @@
 import commonjs from "@rollup/plugin-commonjs";
 import dts from "rollup-plugin-dts";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 
 const packageJson = require("./package.json");
 
-const buildDir = "lib";
+const sharedOutputConfig = {
+  sourcemap: true,
+};
 
 export default [
   {
     input: "src/index.ts",
-    external: ["styled-components"],
-    globals: { "styled-components": "styled" },
+    external: Object.keys(packageJson.peerDependencies),
     output: [
       {
         file: packageJson.main,
         format: "cjs",
-        sourcemap: true,
+        ...sharedOutputConfig,
       },
       {
         file: packageJson.module,
         format: "esm",
-        sourcemap: true,
+        ...sharedOutputConfig,
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
@@ -34,7 +37,6 @@ export default [
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-    external: ["styled-components"],
-    globals: { "styled-components": "styled" },
+    external: Object.keys(packageJson.peerDependencies),
   },
 ];
